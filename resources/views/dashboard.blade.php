a91f1b8926c6d1217c2053a21864b0b752ca6246
JohnBintang69 <111202214399@mhs.dinus.ac.id>
2025-04-02 03:29:01 +0700

    Update Controllers, Public, Resources, and Routes

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>150</h3>
+                <p>New Orders</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2025 <a href="#">AdminLTE</a>.</strong>
+    All rights reserved.
+</footer>
